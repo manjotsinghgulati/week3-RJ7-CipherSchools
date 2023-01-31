5c5e8a75b930efea783ab317dc5e04929b27ad50
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-01-31 23:42:28 +0530

    Event Handling

--- React/first-app/src/components/UserDetails.js
+++ React/first-app/src/components/UserDetails.js
@@ -1,28 +1,39 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
-// const UserDetails = ({user = {} }) => {
-//     return (
-//         <div>
-//             <h3>{user.name}</h3>
-//             <h3>{user.email}</h3>
-//             <h3>{user.phone}</h3>
-//         </div>
-
-//     )
-
-// }
-
-class UserDetails extends Component {
-    user = this.props.user;
-    render() {
-        return (
-            <div>
-                <h3>{this.user.name}</h3>
-                <h3>{this.user.email}</h3>
-                <h3>{this.user.phone}</h3>
+const UserDetails = ({user = {} }) => {
+    const [h1Text, setH1Text] = useState("This is a Demo");
+    // user = props.user;
+    return (
+        <div>
+            <h3>{user.name}</h3>
+            <h3>{user.email}</h3>
+            <h3>{user.phone}</h3>
+            <h1>{h1Text}</h1>
+                <button onClick = {(e) =>{
+                    setH1Text("This is Chipher Schools")
+                }} >Click Me </button>
             </div>
-        )
-    }
+
+    )
+
 }
+
+// class UserDetails extends React.Component {
+//     const [h1Text, setH1Text] = useState("This is a Demo");
+//     user = this.props.user;
+//     render() {
+//         return (
+//             <div>
+//                 <h3>{this.user.name}</h3>
+//                 <h3>{this.user.email}</h3>
+//                 <h3>{this.user.phone}</h3>
+//                 <h1>This is a Demo</h1>
+//                 <button onClick = {(e) =>{
+//                     console.log(e)
+//                 }} >Click Me </button>
+//             </div>
+//         )
+//     }
+// }
 
 export default UserDetails;
