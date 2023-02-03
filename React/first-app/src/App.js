dc5de0eaa513074e57198f1aba179e3043eaee28
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-02-04 00:50:34 +0530

    React Hooks

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import TaskScreen from './components/TaskScreen';
+import CounterComponent from './components/CouterComponent';
 
 const App = () =>{
   return(
@@ -12,6 +14,8 @@ const App = () =>{
         <Routes>  
           <Route path={"/"} element={<h1>This is on Home Page</h1>} />
           <Route path={"/contact"} element={<h1>This is on Conatct Page</h1>} />
+          <Route path={"/tasks"} element={<TaskScreen />} />
+          <Route path={"/counter"} element={<CounterComponent />} />
           {/* <Route path="/about" element={<About />} />
           {/* <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
