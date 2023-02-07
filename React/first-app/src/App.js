039b1e4673ee5aa43f5fa20f2bf15c5b8e41fdbf
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-02-08 00:22:19 +0530

    Lazy Loading

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,36 +1,80 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import TaskScreen from './components/TaskScreen';
-import CounterComponent from './components/CouterComponent';
-import Photos from './components/Photos';
-import higherOrderComponent from './components/Hoc';
-import SampleRenderProps from './components/SampleRenderComponents';
-import I18Component from './components/I18Component';
+import React, { createContext, lazy, Suspense, useContext } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import higherOrderComponent from "./components/Hoc";
+import Header from "./components/Header";
+const SampleRenderProps = lazy(() => import("./components/SampleRenderComponents"));
+const I18Component = lazy(() => import("./components/I18Component"));
+const ErrorBoundary = lazy(() => import("./components/ErrorBoundary"));
+const CounterComponent = lazy(() => import("./components/CouterComponent"));
+const SignUpForm = lazy(() => import("./components/SignUpForm"));
+const SumNumber = lazy(() => import("./components/SumNumber"));
+const TaskScreen = lazy(() => import("./components/TaskScreen"));
+const Photos = lazy(() => import("./components/Photos"));
 
-const App = () =>{
+export const AnupamaContext = createContext({ favCharacter: "Anupama" });
+export const BreakingBadContext = createContext({
+  favCharacter: "Walter White",
+});
+
+const App = () => {
   const TestApp = higherOrderComponent(() => {
-    return <h1>Inside HOC</h1>;
-  })
-  return(
-    <div className="App">
-    <Header />
+    return <div>Test App</div>;
+  });
+
+  const anupamaContent = useContext(AnupamaContext);
+
+  return (
+    <div>
       <BrowserRouter>
-        <Routes>  
-          <Route path={"/"} element={<h1>This is on Home Page</h1>} />
-          <Route path={"/contact"} element={<h1>This is on Conatct Page</h1>} />
-          <Route path={"/tasks"} element={<TaskScreen />} />
-          <Route path={"/counter"} element={<CounterComponent />} />
-          <Route path={"/photos"} element={<Photos />} />
-          <Route path={"/hoc"} element={<TestApp />} />
-          <Route path={"/sample"} element={<SampleRenderProps />} />
-          <Route path={"/international"} element={<I18Component />} />
-          
-        </Routes>
+        <Header />
+        <Suspense fallback={<div>{anupamaContent.favCharacter}</div>}>
+          <Routes>
+            <Route path={"/"} element={<h1>This is on Home Page</h1>} />
+            <Route
+              path={"/contact"}
+              element={<h1>This is on Contact Page</h1>}
+            />
+            <Route path={"/about"} element={<h1>This is on About Page</h1>} />
+            <Route
+              path={"/signup"}
+              element={
+                <ErrorBoundary>
+                  <SignUpForm />
+                </ErrorBoundary>
+              }
+            >
+              <Route path={"sum"} element={<SumNumber />} />
+            </Route>
+            <Route path={"/tasks"} element={<TaskScreen />} />
+            <Route
+              path={"/counter"}
+              element={
+                <ErrorBoundary>
+                  <CounterComponent />
+                </ErrorBoundary>
+              }
+            />
+            <Route path={"/photos"} element={<Photos />} />
+            <Route path={"/test"} element={<TestApp />} />
+            <Route path={"/sample"} element={<SampleRenderProps />} />
+            <Route path={"/international"} element={<I18Component />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App;
+const MyApp = () => (
+  <AnupamaContext.Provider
+    value={{ favCharacter: "Anupama", favVillain: "Vanraj", loudest: "Kavya" }}
+  >
+    <BreakingBadContext.Provider
+      value={{ favCharacter: "Walter White", mostHatedCharacter: "Jessie" }}
+    >
+      <App />
+    </BreakingBadContext.Provider>
+  </AnupamaContext.Provider>
+);
+
+export default MyApp;
